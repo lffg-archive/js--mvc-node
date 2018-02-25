7a8c1742c2abb4ac78884e98669383685e7b171f
Luiz Felipe <frazaolipe@gmail.com>
2018-02-24 23:10:31 -0300

    Correção no caminho.

--- system/router/ext/404-error.js
+++ system/router/ext/404-error.js
@@ -3,14 +3,14 @@ module.exports = function() {
     res.status(404);
     res.format({
       'text/html': () => {
-        const errorConfig = require('../../config/error');
+        const errorConfig = require('../../../config/error');
         res.render(errorConfig['404'].viewPath);
       },
       'application/json': () => {
         res.json({ error: true, status: 404, message: 'Error 404' });
       },
       'default': () => {
-        const errorConfig = require('../../config/error');
+        const errorConfig = require('../../../config/error');
         res.render(errorConfig['404'].viewPath);
       }
     });
--- system/router/ext/general-error.js
+++ system/router/ext/general-error.js
@@ -3,14 +3,14 @@ module.exports = function() {
     res.status(500);
     res.format({
       'text/html': () => {
-        const errorConfig = require('../../config/error');
+        const errorConfig = require('../../../config/error');
         res.render(errorConfig.general.viewPath, { 'error': err.message });
       },
       'application/json': () => {
         res.json({ error: true, status: 500, message: err.message });
       },
       'default': () => {
-        const errorConfig = require('../../config/error');
+        const errorConfig = require('../../../config/error');
         res.render(errorConfig.general.viewPath, { 'error': err.message });
       }
     });
